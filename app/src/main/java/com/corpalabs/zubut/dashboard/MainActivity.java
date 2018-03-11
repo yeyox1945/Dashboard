21b03a8388779e621875a9d65e919330547f2a27
Diego <yeyox1945@gmail.com>
2018-03-10 18:06:59 -0600

    Agregado Slide para agragar ubicacion actual y listView creado

--- app/src/main/java/com/corpalabs/zubut/dashboard/MainActivity.java
+++ app/src/main/java/com/corpalabs/zubut/dashboard/MainActivity.java
@@ -2,6 +2,10 @@ package com.corpalabs.zubut.dashboard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.ebanx.swipebtn.OnStateChangeListener;
+import com.ebanx.swipebtn.SwipeButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SwipeButton swpAddLocation = findViewById(R.id.swpAddLocation);
+
+        swpAddLocation.setOnStateChangeListener(new OnStateChangeListener() {
+            @Override
+            public void onStateChange(boolean active) {
+                Toast.makeText(MainActivity.this, "Ubicacion guardada", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
