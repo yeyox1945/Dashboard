1c6932855f98d531a3ddf1a47d86a210cdb6fb4b
Diego <yeyox1945@gmail.com>
2018-03-10 18:30:25 -0600

    Agregado Slide para agragar ubicacion actual y listView creado

--- app/src/main/java/com/corpalabs/zubut/dashboard/MainActivity.java
+++ app/src/main/java/com/corpalabs/zubut/dashboard/MainActivity.java
@@ -1,7 +1,14 @@
 package com.corpalabs.zubut.dashboard;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.widget.Toast;
 
 import com.ebanx.swipebtn.OnStateChangeListener;
@@ -9,18 +16,55 @@ import com.ebanx.swipebtn.SwipeButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    LocationManager locationManager;
+    LocationListener locationListener;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         SwipeButton swpAddLocation = findViewById(R.id.swpAddLocation);
-
         swpAddLocation.setOnStateChangeListener(new OnStateChangeListener() {
             @Override
             public void onStateChange(boolean active) {
+
+                //Guardar ubicacion actual aqui.....
+
                 Toast.makeText(MainActivity.this, "Ubicacion guardada", Toast.LENGTH_SHORT).show();
             }
         });
+
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Log.i("Location", location.toString());
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+
     }
 }
